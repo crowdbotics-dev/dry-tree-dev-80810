0afa7dcdd860c586c84c57796c24b7a8ab462b29
Crowdbotics <engineering@crowdbotics.com>
2023-05-03 13:22:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-tree-dev-80810",
+  url: "https://dry-tree-dev-80810.botics.co",
+  api: "https://dry-tree-dev-80810.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
